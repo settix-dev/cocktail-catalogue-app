429ba36b7b6e4f26285a646e14092702c4740e45
settix-dev <settimbalamech@gmail.com>
2022-05-02 13:50:08 +0300

    Provide redux store to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import {BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from './routes/Home';
-import Cocktails from './routes/Cocktails';
-import Cocktail from './routes/Cocktail';
-import CocktailDetails from './routes/CocktailDetails';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Cocktails from "./routes/Cocktails";
+import Cocktail from "./routes/Cocktail";
+import CocktailDetails from "./routes/CocktailDetails";
 
 function App() {
   return (
@@ -11,10 +11,10 @@ function App() {
       <h1>Cocktail Catalogue</h1>
       <BrowserRouter>
         <Routes>
-          <Route path='/' exact element={<Home />}></Route>
-          <Route path='/cocktails' element={<Cocktails />}></Route>
-          <Route path='/cocktail' element={<Cocktail />}></Route>
-          <Route path='/cocktailDetails' element={<CocktailDetails />}></Route>
+          <Route path="/" exact element={<Home />}></Route>
+          <Route path="/cocktails" element={<Cocktails />}></Route>
+          <Route path="/cocktail" element={<Cocktail />}></Route>
+          <Route path="/cocktailDetails" element={<CocktailDetails />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
