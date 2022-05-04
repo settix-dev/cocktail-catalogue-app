cbcd709dece7a5de3fc6254954067d6fedca0ac3
settix-dev <settimbalamech@gmail.com>
2022-05-04 22:11:43 +0300

    Add import to index.js

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import cocktailsApiReducer from "../reducers/cocktailsApiReducer";
 import categoryFilterReducer from "./categoryFilterReducer";
+import cocktailApiReducer from "./cocktailApiReducer";
 
 const rootReducer = combineReducers({
   cocktailsCollection: cocktailsApiReducer,
   categoryFilterState: categoryFilterReducer,
+  singleCocktailCollection: cocktailApiReducer,
 });
 
 export default rootReducer;
