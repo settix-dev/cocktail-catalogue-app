bc27f68357802a6d4a0bdb627420af89dd8c36a4
settix-dev <settimbalamech@gmail.com>
2022-05-11 13:30:46 +0300

    Modify fetchCocktails action

--- src/store/actions/cocktailsApiActions.js
+++ src/store/actions/cocktailsApiActions.js
@@ -1,21 +1,19 @@
 const FETCH_COCKTAILS = "FETCH_COCKTAILS";
 const CREATE_COCKTAIL = "CREATE_COCKTAIL";
 
-export const fetchCocktails = (cocktails) => {
-  return async (dispatch) => {
-    try {
-      const response = await fetch(
-        "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
-      );
-      const data = await response.json();
-      dispatch({
-        type: FETCH_COCKTAILS,
-        payload: data,
-      });
-    } catch (err) {
-      console.log(`Whoops!! Something wrong happened: ${err}`);
-    }
-  };
+export const fetchCocktails = async (dispatch) => {
+  try {
+    const response = await fetch(
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a"
+    );
+    const data = await response.json();
+    dispatch({
+      type: FETCH_COCKTAILS,
+      payload: data,
+    });
+  } catch (err) {
+    console.log(`Whoops!! Something wrong happened: ${err}`);
+  }
 };
 
 export const createCocktail = (cocktail) => {
