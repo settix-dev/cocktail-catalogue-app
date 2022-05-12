8e83bfa36a4796525976e059be355b30e2634587
settix-dev <settimbalamech@gmail.com>
2022-05-12 15:43:01 +0300

    Modify filters local states, handlers

--- src/routes/Cocktails.js
+++ src/routes/Cocktails.js
@@ -14,10 +14,10 @@ const cocktailsState = (state) => state.cocktailsCollection; // Retrieve cocktai
 const categoryFilterState = (state) => state.categoryFilterState; // Retrieve Categery filter state from store
 const singleCocktailArrayState = (state) => state.singleCocktailCollection; // Retrieve single cocktails state
 const Cocktails = () => {
-  const [categoryFilter, setCategoryFilter] = useState("");
-  const [glassFilter, setGlassFilter] = useState("");
-  const [alcoholFilter, setAlcoholFilter] = useState("");
-  const [currentFilter, setCurrentFilter] = useState("");
+  const [categoryFilter, setCategoryFilter] = useState("Ordinary Drink");
+  const [glassFilter, setGlassFilter] = useState("Highball glass");
+  const [alcoholFilter, setAlcoholFilter] = useState("Alcoholic");
+  const [drinkFilter, setDrinkFilter] = useState("All"); // Tracks the current filter
   const cocktails = useSelector(cocktailsState).cocktails;
   const dispatch = useDispatch();
   const currentCategoryFilter = useSelector(categoryFilterState).drinkCategory;
@@ -31,21 +31,22 @@ const Cocktails = () => {
 
   const handleCategoryChangeFilter = (e) => {
     console.log(`Category filter: ${e.target.value}`);
-    setCategoryFilter(e.target.value === "All" ? "" : e.target.value);
-    setCurrentFilter(e.target.value === "All" ? "" : e.target.value);
+    setCategoryFilter(e.target.value);
   };
 
   const handleGlassChangeFilter = (e) => {
     console.log(`Glass Filter: ${e.target.value}`);
-    setGlassFilter(e.target.value === "All" ? "" : e.target.value);
-    setCurrentFilter(e.target.value === "All" ? "" : e.target.value);
+    setGlassFilter(e.target.value);
   };
 
   const handleAlcoholChangeFilter = (e) => {
     console.log(`Alcohol Filter: ${e.target.value}`);
-    setAlcoholFilter(e.target.value === "All" ? "" : e.target.value);
-    setCurrentFilter(e.target.value === "All" ? "" : e.target.value);
+    setAlcoholFilter(e.target.value);
   };
+
+  const handleDrinkFilterChange = (e) => {
+    setDrinkFilter(e.target.value);
+  }
 
   useEffect(() => {
     if (cocktails.length === 0) {
@@ -70,6 +71,12 @@ const Cocktails = () => {
   return (
     <div>
       <h1>Cocktails</h1>
+      <p>Please select a cocktail</p>
+      <select onChange={handleDrinkFilterChange}>
+        <option value="All">All</option>
+        <option value="Filter">Drink Filters</option>
+      </select>
+      {drinkFilter !== "All" && 
       <CocktailFilters
         categoryFilter={categoryFilter}
         glassFilter={glassFilter}
@@ -77,42 +84,26 @@ const Cocktails = () => {
         handleAlcoholChangeFilter={handleAlcoholChangeFilter}
         handleCategoryChangeFilter={handleCategoryChangeFilter}
         handleGlassChangeFilter={handleGlassChangeFilter}
-      />
+      />}
       {cocktails.length !== 0 ? (
-        currentFilter === "" ? (
-          cocktails.map((cocktail, index) => (
-            <div key={index}>
-              {cocktail.drinks.map((drink, index) => (
+        cocktails.map((cocktail, index) => (
+          <div key={index}>
+            {(drinkFilter === "All" ? cocktail.drinks : cocktail.drinks
+              .filter(
+                (drink) =>
+                  alcoholFilter === drink.strAlcoholic &&
+                  glassFilter === drink.strGlass &&
+                  categoryFilter === drink.strCategory
+              ))
+              .map((drink, index) => (
                 <div key={index}>
-                  {drink.idDrink}: {drink.strGlass}, {drink.strCategory}
+                  {drink.idDrink}: {drink.strGlass}, {drink.strDrinkThumb},{" "}
                   <img src={`${drink.strDrinkThumb}`} alt="Cocktail Thumb" />
-                  <Link to={`/cocktail/${drink.strDrink}`}>
-                    {drink.strDrink}
-                  </Link>
+                  <Link to={`/cocktail/${drink.strDrink}`}>{drink.strDrink}</Link>
                 </div>
               ))}
-            </div>
-          ))
-        ) : (
-          cocktails.map((cocktail, index) => (
-            <div key={index}>
-              {cocktail.drinks
-                .filter(
-                  (drink, index) =>
-                    currentFilter === drink.strAlcoholic ||
-                    currentFilter === drink.strGlass ||
-                    currentFilter === drink.strCategory
-                )
-                .map((drink, index) => (
-                  <div key={index}>
-                    {drink.idDrink}: {drink.strGlass}, {drink.strDrinkThumb},{" "}
-                    <img src={`${drink.strDrinkThumb}`} alt="Cocktail Thumb" />
-                    <Link to={"/cocktail"}>{drink.strDrink}</Link>
-                  </div>
-                ))}
-            </div>
-          ))
-        )
+          </div>
+        ))
       ) : (
         <div>
           <img src={loader} alt="Loading" />
